084921af44ce99d74ff686dfce5fe25df387ad57
Billy Chan <bill0930@gmail.com>
2021-04-07 03:15:06 +0800

    add assembly

---
+++ {name}Assembly.swift
@@ -0,0 +1,13 @@
+import Foundation
+import Swinject
+import SwinjectAutoregistration
+
+class {name}ModuleAssembly: Assembly {
+    func assemble(container: Container) {
+        container.autoregister({name}RouterInterface.self, initializer: {name}Router.init)
+        container.autoregister({name}PresenterInterface.self, initializer: {name}Presenter.init)
+        container.autoregister({name}InteractorInterface.self, initializer: {name}Interactor.init)
+        container.autoregister({name}ModuleInterface.self, initializer: {name}Module.init)
+        container.autoregister({name}ViewInterface.self, initializer: {name}View.init as () -> {name}View)
+    }
+}
